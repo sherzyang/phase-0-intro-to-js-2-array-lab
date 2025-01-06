a0f13bcb9c58dd0e6291667b967ae75a884c7cae
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-05 16:51:30 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+cats;
+
+function destructivelyAppendCat(name) {
+    console.log(cats.push(name));
+    console.log(cats);
+}
+
+function destructivelyPrependCat(name) {
+    console.log(cats.unshift(name));
+    console.log(cats);
+}
+
+function destructivelyRemoveLastCat() {
+    console.log(cats.pop());
+    console.log(cats);
+}
+
+function destructivelyRemoveFirstCat() {
+    console.log(cats.shift());
+    console.log(cats);
+}
+
+function appendCat(name) { 
+    let newCats = [...cats];
+    console.log(newCats.push(name));
+    return newCats;
+}
+
+function prependCat(name) { 
+    let newCats = [...cats];
+    console.log(newCats.unshift(name));
+    return newCats;
+}
+
+function removeLastCat() { 
+    let newCats = [...cats];
+    console.log(newCats.pop());
+    return newCats;
+}
+
+function removeFirstCat() { 
+    let newCats = [...cats];
+    console.log(newCats.shift());
+    return newCats;
+}
